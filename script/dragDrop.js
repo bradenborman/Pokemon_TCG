04feafebb143bacbf4d1f892bf75bd8ddbc01f99
Brady Borman <bradenborman@hotmail.com>
2018-09-22 20:31:03 -0500

    Draw card working and random loads hand

--- script/dragDrop.js
+++ script/dragDrop.js
@@ -1,4 +1,6 @@
 
+
+
  var isCardEmpty_ONE = true;
  var isCardEmpty_TWO = true;
  var ACTIVE_POKEMON = null
@@ -8,7 +10,12 @@
  var Beedrill = new Pokemon("Beedrill", 80, "Fire", 0, 32, "Twineedle", "Posion Sting");
  var Blastoise = new Pokemon("Blastoise", 100, "Lightning", 3, 52, "POK\'E POWER", "Hydro Pump");
  var Hitmonchan = new Pokemon("Hitmonchan", 70, "Psychic", 2, 33, "Jab", "Special Punch");
- 
+ var Pidgeotto = new Pokemon("Pidgeotto", 60, "Lightning", 1, 36, "Whirlwind", "Mirror Move");
+ var Alakazam = new Pokemon("Alakazam", 80, "Psychic", 3, 42, "POK\'E POWER", "Confuse Ray");
+ var Gyarados = new Pokemon("Gyarados", 100, "Grass", 3, 41, "Dragon Rage", "Bubblebeam");
+ var Ninetales = new Pokemon("Ninetales", 80, "Water", 1, 32, "Lure", "Fire Blast");
+ var Mewtwo = new Pokemon("Mewtwo", 60, "Psychic", 3, 53, "Psychic", "Barrier"); 
+ var Jigglypuff = new Pokemon("Jigglypuff", 60, "Fighting", 1, 14, "Lullaby", "Pound"); 
 
 function Pokemon(name, hp, weakness, retreat, level, move1, move2) {
     this.name = name;
@@ -37,11 +44,28 @@ function Pokemon(name, hp, weakness, retreat, level, move1, move2) {
 	else if(name == Raichu.name)
 		return Raichu
 	
+	else if(name == Pidgeotto.name)
+		return Pidgeotto
+	
+	else if(name == Alakazam.name)
+		return Alakazam
+	
+	else if(name == Gyarados.name)
+		return Gyarados
+	
+	else if(name == Ninetales.name)
+		return Ninetales
+	
+	else if(name == Mewtwo.name)
+		return Mewtwo
+	
+	else if(name == Jigglypuff.name)
+		return Jigglypuff
+	
 	else
 		return null
 	
 }
- 
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -65,8 +89,8 @@ function drop1(ev) {
 		
 		$(".INFO_ONE").html(Display);
 		ACTIVE_POKEMON = Pokemon
+		closeNav()
 	}
-	
 	clearHand()
 }
 
@@ -79,13 +103,7 @@ function clearHand() {
 		$(this).css("display", "none");
 	}
 });
-	
-	
 }
-
-
-
-
 
 function Fight1() {
 	alert(ACTIVE_POKEMON.move1)
@@ -95,10 +113,6 @@ function Fight2() {
 	alert(ACTIVE_POKEMON.move2)
 }
 
-
-
-
- 
 function faintPlayer(player) {
 var cardHolder = null;
 	
@@ -116,15 +130,51 @@ var cardHolder = null;
 }
 
 
+ var DECK = [
+	'<img id="card1" name="Charizard" ondragstart="drag(event)" draggable="true" src="imgs/charizard.jpg" />',
+	'<img id="card2" name="Raichu" ondragstart="drag(event)" draggable="true" src="imgs/raichu.jpg" />',
+	'<img id="card3" name="Beedrill" ondragstart="drag(event)" draggable="true" src="imgs/beedrill.jpg" />',
+	'<img id="card4" name="Blastoise" ondragstart="drag(event)" draggable="true" src="imgs/blastoise.jpg" />',
+	'<img id="card5" name="Hitmonchan" ondragstart="drag(event)" draggable="true" src="imgs/hitmonchan.jpg" />',
+	'<img id="card6" name="Pidgeotto" ondragstart="drag(event)" draggable="true" src="imgs/pidgeotto.jpg" />',
+	'<img id="card7" name="Alakazam" ondragstart="drag(event)" draggable="true" src="imgs/alakazam.jpg" />',
+	'<img id="card8" name="Gyarados" ondragstart="drag(event)" draggable="true" src="imgs/gyarados.jpg" />',
+	'<img id="card9" name="Ninetales" ondragstart="drag(event)" draggable="true" src="imgs/ninetales.jpg" />',
+	'<img id="card10" name="Mewtwo" ondragstart="drag(event)" draggable="true" src="imgs/mewtwo.jpg" />',
+	'<img id="card11" name="Jigglypuff" ondragstart="drag(event)" draggable="true" src="imgs/jigglypuff.jpg" />'
+]
+
+
+function loadHand() {
+	
+		$(".hand").eq(0).html(getCard())
+		$(".hand").eq(1).html(getCard())
+		$(".hand").eq(2).html(getCard())
+		$(".hand").eq(3).html(getCard())
+		$(".hand").eq(4).html(getCard())
+}
+
+
+function getCard() {
+	var length = (DECK.length);
+	var toPick = Math.floor((Math.random() * length) + 0);
+	var toReturn = DECK[toPick]
+	DECK.splice(toPick, 1);
+	return toReturn
+}
+
+loadHand()
 
 
 
 
-
-
-
-
-
+function draw(player) {
+	
+		if(player == 1 && DECK.length > 0)
+			document.getElementById("CARDS_IN_HAND").innerHTML += '<div class="hand">' + getCard() +  '</div>'
+		
+		
+}
 
 
 
